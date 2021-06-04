c7e2ae074a96c59e420d1155e7272c995a65ca7a
KoukiNAGATA <koukinagata.academic02@gmail.com>
2021-06-04 18:02:51 +0900

    create blog

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(
+        "タイトル", max_length=50,
+    )
+    body = models.TextField("本文")
+    created_at = models.DateTimeField(
+        "作成日時", auto_now_add=True,
+    )
