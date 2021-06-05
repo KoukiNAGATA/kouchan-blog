270d310a1022646418fac94825e518a718b98f4b
KoukiNAGATA <koukinagata.academic02@gmail.com>
2021-06-05 19:02:06 +0900

    [feat]add markdown settings

--- kouchan-blog/urls.py
+++ kouchan-blog/urls.py
@@ -15,10 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from django.conf.urls.static import static
+from django.conf import settings
 from blog.views import PostDetailView, PostListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/<int:post_id>/', PostDetailView.as_view(), name="post_detail"),
     path('', PostListView.as_view(), name="post_list"),
+    url(r'mdeditor/', include('mdeditor.urls'))
 ]
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
