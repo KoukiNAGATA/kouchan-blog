7b3a7e9d9348de71b64000cf0c51f1eeef060454
KoukiNAGATA <koukinagata.academic02@gmail.com>
2021-06-09 15:30:17 +0900

    edit path

--- kouchan-blog/settings.py
+++ kouchan-blog/settings.py
@@ -110,8 +110,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SASS_PROCESSOR_INCLUDE_FILE_PATTERN = r'^.+\.scss$'
 SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
