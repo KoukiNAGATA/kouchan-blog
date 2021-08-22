a93d51e3c120cde06da3a15027e2c062436aa31f
KoukiNAGATA <koukinagata.academic02@gmail.com>
2021-08-22 19:25:38 +0900

    reject domain

--- kouchan-blog/settings.py
+++ kouchan-blog/settings.py
@@ -141,7 +141,7 @@ DATABASES = {
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['admin.koukinagata.info', 'koukinagata.info',
-                 'kouchan-blog.herokuapp.com', '127.0.0.1', 'admin.localhost', 'localhost']
+                 '127.0.0.1', 'admin.localhost', 'localhost']
 DEBUG = False
 # Apply local settings
 try:
